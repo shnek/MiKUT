75643c2216fd6a30995dc83006c39464c9a9268b
shnek <shnek94@gmail.com>
2015-11-09 12:46:32 +0100

    Added artificial classes to make folders visible on git

---
+++ pl.edu.agh.to2/src/main/java/BillingReader/delete_this_class.java
@@ -0,0 +1,7 @@
+package BillingReader;
+
+/**
+ * Created by kuba on 09.11.15.
+ */
+public class delete_this_class {
+}
---
+++ pl.edu.agh.to2/src/main/java/CostCalculator/delete_this_class.java
@@ -0,0 +1,7 @@
+package CostCalculator;
+
+/**
+ * Created by kuba on 09.11.15.
+ */
+public class delete_this_class {
+}
