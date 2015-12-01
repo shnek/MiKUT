842bc1dc68a574577a2629250b4fedabe491f8d3
wojciech <wojciech.radwanski@gmail.com>
2015-12-01 10:34:24 +0100

    Adding Orange, PLlus tests

--- pl.edu.agh.to2/src/test/java/OperatorResolver/NumberVeryficatorTest.java
+++ pl.edu.agh.to2/src/test/java/OperatorResolver/NumberVeryficatorTest.java
@@ -1,6 +1,8 @@
 package OperatorResolver;
 
 import OperatorResolver.operatorresolver.Operator;
+import OperatorResolver.veryficator.NumberVeryficator;
+import OperatorResolver.veryficator.Veryficator;
 import junit.framework.TestCase;
 
 /**
@@ -9,20 +11,30 @@ import junit.framework.TestCase;
 public class NumberVeryficatorTest extends TestCase{
 
     public void testPlayVerify(){
-        OperatorResolver.veryficator.NumberVeryficator nv = new OperatorResolver.veryficator.NumberVeryficator();
+        Veryficator nv = new NumberVeryficator();
         assertEquals(Operator.PLAY, nv.verify("792600000"));
     }
     public void testHeyahVerify(){
-        OperatorResolver.veryficator.NumberVeryficator nv = new OperatorResolver.veryficator.NumberVeryficator();
+        Veryficator nv = new NumberVeryficator();
         assertEquals(Operator.OTHERPOLAND, nv.verify("888888888"));
     }
 
     public void testTmobileVerify(){
-        OperatorResolver.veryficator.NumberVeryficator nv = new OperatorResolver.veryficator.NumberVeryficator();
+        Veryficator nv = new NumberVeryficator();
         assertEquals(Operator.TMOBILE, nv.verify("608800000"));
     }
+    //510100100 Orange
+//        664097160 PTC / T-Mobile
+//        501936076 P4
+//        695 416 939 Plus
 
+    public void testOrangeVerify(){
+        Veryficator nv = new NumberVeryficator();
+        assertEquals(Operator.ORANGE, nv.verify("510100100"));
+    }
 
-
-
+    public void testPlusVer(){
+        Veryficator nv = new NumberVeryficator();
+        assertEquals(Operator.PLUS, nv.verify("695416939"));
+    }
 }
