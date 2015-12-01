8ce1389dd68634d2b1cb2973c420864425978f02
shnek <shnek94@gmail.com>
2015-12-01 10:22:25 +0100

    Refactoring code.

--- pl.edu.agh.to2/src/test/java/OperatorResolver/NumberVeryficatorTest.java
+++ pl.edu.agh.to2/src/test/java/OperatorResolver/NumberVeryficatorTest.java
@@ -8,19 +8,6 @@ import junit.framework.TestCase;
  */
 public class NumberVeryficatorTest extends TestCase{
 
-    public void testHeyahVerifyOld(){
-        NumberVeryficator nv = new NumberVeryficator();
-        assertEquals("Heyah", nv.verify("888888888"));
-    }
-    public void testTmobileVerifyOld(){
-        NumberVeryficator nv = new NumberVeryficator();
-        assertEquals("PTC / T-Mobile", nv.verify("608800000"));
-    }
-    public void testPlayVerifyOld(){
-        NumberVeryficator nv = new NumberVeryficator();
-        assertEquals("P4", nv.verify("792600000"));
-    }
-
     public void testPlayVerify(){
         OperatorResolver.veryficator.NumberVeryficator nv = new OperatorResolver.veryficator.NumberVeryficator();
         assertEquals(Operator.PLAY, nv.verify("792600000"));
