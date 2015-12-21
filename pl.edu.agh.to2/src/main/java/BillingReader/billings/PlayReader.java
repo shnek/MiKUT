954e93f4ef7bdad372c908170d8566d2abd3a2e1
krzysiekk1 <krzysiekk1.krakow@gmail.com>
2015-12-21 11:53:17 +0100

    prawidłowy billing Play

--- pl.edu.agh.to2/src/main/java/BillingReader/billings/PlayReader.java
+++ pl.edu.agh.to2/src/main/java/BillingReader/billings/PlayReader.java
@@ -21,54 +21,67 @@ public class PlayReader extends BillingReader {
     public Billing readBilling(File file) throws FileNotFoundException {
 
         if (!extensionCorrect(playExtension,file)) {
-            //TO DO LATER
+            //TODO
         }
 
         Scanner scanner = new Scanner(file);
-        String[] elements;
+        readFile(scanner);
 
-        while (scanner.hasNext()) {
-            elements = scanner.next().split(",");
-            if (!elements[1].equals("Wychodzące")) continue;
-
-            switch(elements[2].toLowerCase()) {
-                case "rozmowy głosowe":
-                    //addDial()
-                    break;
-                case "sms":
-                    //addSms()
-                    break;
-                case "mms":
-                    //addMms
-                    break;
-                case "dane":
-                    //addTransfer
-                    break;
-                default:
-                    //ignore
-                    break;
-            }
-        }
-
-        BillingLists lists = new BillingLists(dialList,smsList,mmsList,transferList);
+        BillingLists lists = new BillingLists(dialList, smsList, mmsList, transferList);
         OperatorResolverImp resolver = new OperatorResolverImp(lists);
         return resolver.getBilling();
     }
 
-    private void addDial(String number, int lengthInSeconds) {
-        //todo
+    private void readFile(Scanner scanner) {
+
+        String phoneNr;
+        int length;
+        int kB;
+
+        while (scanner.hasNext()) {
+            String[] elements = scanner.next().split(",");
+
+            if (!elements[1].equals("Wychodzące")) continue;
+            switch(elements[2].toLowerCase()) {
+                case "rozmowy głosowe":
+                    phoneNr = elements[5];
+                    length = toSeconds(elements[7]);
+                    Dial dial = new Dial(phoneNr, length);
+                    dialList.add(dial);
+                    break;
+                case "sms":
+                    phoneNr = elements[5];
+                    Sms sms = new Sms(phoneNr);
+                    smsList.add(sms);
+                    break;
+                case "mms":
+                    phoneNr = elements[5];
+                    Mms mms = new Mms(phoneNr);
+                    mmsList.add(mms);
+                    break;
+                case "dane":
+                    kB = tokBytes(elements[7]);
+                    Transfer transfer = new Transfer(kB);
+                    transferList.add(transfer);
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 
-    private void addSms() {
-        //todo
+    private int toSeconds(String text) {
+        String withColon = text.substring(0,5);
+        String minutes = withColon.substring(0,2);
+        String seconds = withColon.substring(3);
+        int allSeconds = Integer.parseInt(minutes)*60 + Integer.parseInt(seconds);
+        return allSeconds;
     }
 
-    private void addMms() {
-        //todo
-    }
-
-    private void addTransfer() {
-        //todo
+    private int tokBytes(String text) {
+        String kBytes = text.split(",")[0];
+        int kB = Integer.parseInt(kBytes);
+        return kB;
     }
 
 }
