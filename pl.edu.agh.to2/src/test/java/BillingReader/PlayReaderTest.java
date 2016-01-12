2160a7974e853c4d7f46c8106ae053727871bc78
krzysiekk1 <krzysiekk1.krakow@gmail.com>
2016-01-12 10:21:04 +0100

    poprawione testy

--- pl.edu.agh.to2/src/test/java/BillingReader/PlayReaderTest.java
+++ pl.edu.agh.to2/src/test/java/BillingReader/PlayReaderTest.java
@@ -2,8 +2,8 @@ package BillingReader;
 
 import BillingReader.billings.PlayReader;
 import org.junit.Test;
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertNull;
+
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 
 public class PlayReaderTest {
@@ -15,18 +15,18 @@ public class PlayReaderTest {
 
     @Test
     public void testToSeconds() {
-        playReader = mock(PlayReader.class);
+        playReader = new PlayReader();
         text = "02:33 min.,,";
-        assertNotEquals(playReader.toSeconds(text), 153);
+        assertEquals(153, playReader.toSeconds(text));
     }
 
-    /*@Test
+    @Test
     public void testParseLine() {
         playReader = mock(PlayReader.class);
         line = "27,Wychodz�ce,Rozmowy g�osowe,2015.11.28,18:18:49,48725035819,G�OSOWE,02:33 min.,,";
         elements = line.split(",");
         playReader.parseLine(elements);
-        assertNull(playReader.getDialList());
-    }*/
+        assertNotNull(playReader.getDialList());
+    }
 
 }
