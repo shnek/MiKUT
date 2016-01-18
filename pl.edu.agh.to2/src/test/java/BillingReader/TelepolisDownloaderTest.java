bad98fe30930cd81b1f62c212e92f1154b8209a2
superszak <superszak@wp.pl>
2016-01-18 14:34:45 +0100

    zmiana testów na takie które przechodzą hurra!!!!!

--- pl.edu.agh.to2/src/test/java/BillingReader/TelepolisDownloaderTest.java
+++ pl.edu.agh.to2/src/test/java/BillingReader/TelepolisDownloaderTest.java
@@ -1,12 +1,12 @@
 package BillingReader;
 
 import BillingReader.offers.Offer;
-import BillingReader.offers.telepolis.setters.AttributeSetter;
-import BillingReader.offers.telepolis.setters.InnerCallSetter;
-import BillingReader.offers.telepolis.setters.InternetFieldsSetter;
+import BillingReader.offers.telepolis.setters.*;
 import org.jsoup.nodes.Element;
 import org.jsoup.parser.Tag;
 import org.junit.Test;
+
+import java.math.BigDecimal;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
@@ -21,101 +21,109 @@ public class TelepolisDownloaderTest {
 
     @Test
     public void testInnerCall() {
-        setter = mock(InnerCallSetter.class);
+        setter = new InnerCallSetter();
         offer = new Offer();
         element1 = "<td>Koszt minuty do własnej sieci</td>";
-        element2 = new Element(tag,"<td>8,50 zł</td>");
+        element2 = new Element(tag,"url");
+        element2.appendText("0,09 zł");
         if (setter.matchesPattern(element1)) {
             setter.setAttribute(offer,element2);
         }
-        assertNotEquals(offer.getInnerCallCost(), 8.5);
+        assertEquals(new BigDecimal("0.09"), offer.getInnerCallCost());
     }
 
     @Test
     public void testOuterCall() {
-        setter = mock(InnerCallSetter.class);
+        setter = new OuterCallSetter();
         offer = new Offer();
         element1 = "<td>Koszt minuty do pozostałych sieci</td>";
-        element2 = new Element(tag,"<td>8,50 zł</td>");
+        element2 = new Element(tag,"url");
+        element2.appendText("8,50 zł");
         if (setter.matchesPattern(element1)) {
             setter.setAttribute(offer,element2);
         }
-        assertNotEquals(offer.getOuterCallCost(), 8.5);
+        assertEquals(new BigDecimal("8.50"),offer.getOuterCallCost());
     }
 
     @Test
     public void testInnerSms() {
-        setter = mock(InnerCallSetter.class);
+        setter = new InnerSmsSetter();
         offer = new Offer();
         element1 = "<td>Opłata za SMS do własnej sieci</td>";
-        element2 = new Element(tag,"<td>8,50 zł</td>");
+        element2 = new Element(tag,"url");
+        element2.appendText("8,50 zł");
         if (setter.matchesPattern(element1)) {
             setter.setAttribute(offer,element2);
         }
-        assertNotEquals(offer.getInnerSmsCost(), 8.5);
+        assertEquals(new BigDecimal("8.50"), offer.getInnerSmsCost());
     }
 
     @Test
     public void testOuterSms() {
-        setter = mock(InnerCallSetter.class);
+        setter = new OuterSmsSetter();
         offer = new Offer();
         element1 = "<td>Opłata za SMS do pozostałych sieci</td>";
-        element2 = new Element(tag,"<td>8,50 zł</td>");
+        element2 = new Element(tag,"url");
+        element2.appendText("8,50 zł");
         if (setter.matchesPattern(element1)) {
             setter.setAttribute(offer,element2);
         }
-        assertNotEquals(offer.getOuterSmsCost(), 8.5);
+        assertEquals(new BigDecimal("8.50"), offer.getOuterSmsCost());
     }
 
     @Test
     public void testInnerMms() {
-        setter = mock(InnerCallSetter.class);
+        setter = new InnerMmsSetter();
         offer = new Offer();
         element1 = "<td>Opłata za MMS do własnej sieci</td>";
-        element2 = new Element(tag,"<td>8,50 zł</td>");
+        element2 = new Element(tag,"url");
+        element2.appendText("8,50 zł");
         if (setter.matchesPattern(element1)) {
             setter.setAttribute(offer,element2);
         }
-        assertNotEquals(offer.getInnerMmsCost(), 8.5);
+        assertEquals(new BigDecimal("8.50"), offer.getInnerMmsCost());
     }
 
     @Test
     public void testOuterMms() {
-        setter = mock(InnerCallSetter.class);
+        setter = new OuterMmsSetter();
         offer = new Offer();
-        element1 = "<td>Opłata za SMS do pozostałych sieci</td>";
-        element2 = new Element(tag,"<td>8,50 zł</td>");
+        element1 = "<td>Opłata za MMS do pozostałych sieci</td>";
+        element2 = new Element(tag,"url");
+        element2.appendText("8,50 zł");
         if (setter.matchesPattern(element1)) {
             setter.setAttribute(offer,element2);
         }
-        assertNotEquals(offer.getOuterMmsCost(), 8.5);
+        assertEquals(new BigDecimal("8.50"), offer.getOuterMmsCost());
     }
 
     @Test
     public void testInternetMbCost() {
-        setter = mock(InternetFieldsSetter.class);
+        setter = new InternetFieldsSetter();
         offer = new Offer();
         element1 = "<td colspan=\"3\" class=\"header\">Pakiet internetowy</td>";
-        String message = "<td colspan=\"3\" class=\"long\">0,01 zł/20 kB;" +
+        String message = "0,01 zł/20 kB;" +
                 " możliwość wykupienia pakietu &quot;NetGT 500 MB&quot; za 6,99 zł/30 dni," +
-                " &quot;NetGT 1 GB&quot; za 7,99 zł/30dni lub &quot;NetGT 4GB&quot; za 14,99 zł/30 dni</td>";
-        element2 = new Element(tag,message);
+                " &quot;NetGT 1 GB&quot; za 7,99 zł/30dni lub &quot;NetGT 4GB&quot; za 14,99 zł/30 dni";
+        element2 = new Element(Tag.valueOf("td colspan"),"url");
+        element2.appendText(message);
         if (setter.matchesPattern(element1)) {
             setter.setAttribute(offer,element2);
         }
-        assertNotEquals(offer.getInternetMbCost(), 0.5);
+        assertEquals(new BigDecimal("0.5"), offer.getInternetMbCost());
     }
 
     @Test
     public void testFreeInternetAmount() {
-        setter = mock(InternetFieldsSetter.class);
+        setter = new InternetFieldsSetter();
         offer = new Offer();
         element1 = "<td colspan=\"3\" class=\"header\">Pakiet internetowy</td>";
-        element2 = new Element(tag,"<td class=\"pakiet\">12 GB</td>");
+        element2 = new Element(tag,"url");
+        element2.appendText("12 GB");
         if (setter.matchesPattern(element1)) {
             setter.setAttribute(offer,element2);
         }
-        assertNotSame(offer.getFreeInternetMb(), 12000.0);
+        assertEquals(Double.valueOf(12000.0), Double.valueOf(offer.getFreeInternetMb()));
     }
 
 }
